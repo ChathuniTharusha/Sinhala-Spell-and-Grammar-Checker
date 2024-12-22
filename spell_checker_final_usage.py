211bf1703faddf0514f418ad3128531dab8ceb4c
Samarasekara S.M.R.Y <2020e196@eng.jfn.ac.lk>
2024-12-22 19:08:54 +0530

    spell checker

---
+++ spell_checker_final_usage.py
@@ -0,0 +1,7 @@
+from spell_checker_final import check_spelling_and_print  # Import the function
+
+# Example paragraph to check spelling
+paragraph = "මෙය අරමුන අතිශය වැදගත් වෙයි. අපිට ඉතා හොඳ අය මෙන්ම අලුත් වැඩේ ගැන හිතනවා."
+
+# Call the function to check and print spelling suggestions
+check_spelling_and_print(paragraph)
